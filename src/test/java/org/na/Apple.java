50d0dae005b0aa309a5d0763aa8d8e4c6e9ac6a8
karthiksnipper <karthik.lifeissweet@gmail.com>
2023-01-22 16:10:52 +0530

    first upload

---
+++ src/test/java/org/na/Apple.java
@@ -0,0 +1,21 @@
+package org.na;
+
+public class Apple {
+
+	private void Meth() { System.out.println("m1");
+
+	}
+	
+	private void cryst() {
+		System.out.println("m2");
+	}
+	
+	private void blueStone() {
+		System.out.println("m3");
+	}
+	
+	
+	
+	
+	
+}
